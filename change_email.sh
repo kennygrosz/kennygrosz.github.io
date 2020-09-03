03206c55fcb7a53a233f472ebcc63cf4336eb236
Kenny Groszman <kennygrosz@gmail.com>
2020-09-03 14:51:22 -0400

    resolve merge conflict

--- change_email.sh
+++ change_email.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-git filter-branch --env-filter '
-if [ "$GIT_COMMITTER_EMAIL" = "kennygros@gmail.com" ] 
-then
-	export GIT_COMMITTER_NAME="Kenny Groszman"
-	export GIT_COMMITTER_EMAIL="kennygrosz@gmail.com" 
+git filter-branch --env-filter '  
+OLD_EMAIL="kennygros@gmail.com" 
+CORRECT_NAME="Kenny Groszman" 
+CORRECT_EMAIL="kennygrosz@gmail.com" 
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] 
+then 
+	export GIT_COMMITTER_NAME="$CORRECT_NAME" 
+	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" 
 fi 
-if [ "$GIT_AUTHOR_EMAIL" = "kennygros@gmail.com" ] 
-then
-	export GIT_AUTHOR_NAME="Kenny Groszman"
-	export GIT_AUTHOR_EMAIL="kennygrosz@gmail.com"
-
-fi
-' --tag-name-filter-cat -- --branches --tags
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] 
+then 
+	export GIT_AUTHOR_NAME="$CORRECT_NAME" 
+	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" 
+fi 
+' --tag-name-filter cat -- --branches --tags
